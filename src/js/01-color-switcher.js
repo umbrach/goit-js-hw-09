41d0fb7afa6cc6e9024db468982b5c5ecbf82e2a
Denys Barabanov <umbrach415@gmail.com>
2022-10-19 00:25:58 +0300

    Add start/stop btn 'colorchanger' function

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+console.log(startBtn);
+console.log(stopBtn);
+
+
+const startAction = startBtn.addEventListener('click', getRandomHexColor);
+const stopAction = stopBtn.addEventListener('click', getRandomHexColor);
+
+// console.log(startAction);
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// // console.log(getRandomHexColor());
