caa7c6d232940cce6b9efc58a3282058e302052c
Denys Barabanov <umbrach415@gmail.com>
2022-10-21 17:48:01 +0300

    Add promises js code

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+  promise
+    .then(value => Notiflix.Notify.success(value))
+    .catch(error => Notiflix.Notify.failure(error));
+}
+
+const onSubmitBtnClick = document.querySelector('.form');
+onSubmitBtnClick.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  let delayEl = Number(document.querySelector('input[name="delay"]').value);
+  let amountEl = Number(document.querySelector('input[name="amount"]').value);
+  let stepEl = Number(document.querySelector('input[name="step"]').value);
+  for (let i = 0; i < amountEl; i += 1) {
+    let promiseValue = i + 1;
+    let promiseDelay = delayEl + stepEl * i;
+    createPromise(promiseValue, promiseDelay);
   }
 }
